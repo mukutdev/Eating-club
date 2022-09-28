fdb89c8b359bf83f25be09b37102d15d33df00ab
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-09-29 03:18:45 +0600

    all-cards-rendered

---
+++ src/components/SingleTodo/SingleTodo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './SingleTodo.css'
+const SingleTodo = ({todo}) => {
+    const {image , name, description , age , time} = todo
+    console.log(name);
+    return (
+        <div className='todo-card'>
+            <div className="todo-img">
+                <img src={image} alt="" />
+            </div>
+            <div className="todo-info">
+                <h3>{name}</h3>
+                <p>{description}</p>
+                <h4>For Age : {age}</h4>
+                <h4>Time required : {time}</h4>
+            </div>
+            <div className="todo-btn">
+                <button className='btn'>Add To List</button>
+            </div>
+        </div>
+    );
+};
+
+export default SingleTodo;
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Todo.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDumbbell } from '@fortawesome/free-solid-svg-icons'
+import SingleTodo from '../SingleTodo/SingleTodo';
 const Todo = () => {
-        
 
+    const [todos, setTodo] = useState([])
+
+    useEffect(() =>{
+
+            fetch('data.json')
+            .then(res => res.json())
+            .then(data => setTodo(data))
+
+    }, [])
 
     return (
         <div className='todo-container'>
-             <div className="todo-list">
+             <div className="logo">
                     <h1> <FontAwesomeIcon className='icon-dumb' icon={faDumbbell} /> Fitness Club</h1>
+
+                    <div className='todo-wrapper'>
+                            {
+                                todos.map((todo) => <SingleTodo
+                                
+                                key={todo.id}
+                                todo={todo}                                
+                                >
+                                </SingleTodo> )
+                            }
+                    </div>
              </div>
               <div className="todo-stats">
              <h2>world</h2>
