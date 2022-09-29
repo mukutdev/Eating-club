c1ffe975260d43cbd46e7b35bf30ed60c4b93355
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-09-29 16:55:47 +0600

    break-time-addded

--- src/components/TodoStats/TodoStats.js
+++ src/components/TodoStats/TodoStats.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TodoStats.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationPin } from '@fortawesome/free-solid-svg-icons'
@@ -11,8 +11,14 @@ const TodoStats = ({times}) => {
         istimateTime = istimateTime + time.time
     }
 
+    const [breakTime , setBreakTime] = useState(0)
+
+    const handleBreakTime = (value)=>{
+        setBreakTime(value)
+    }
+
     return (
-        <div>
+        <div className='todos-wrapper'>
             <div className="person-info">
                 <img src={Person} alt="" />
                  <h3>James Bond</h3>
@@ -38,11 +44,11 @@ const TodoStats = ({times}) => {
              <div className="break-info">
                 <h2>Add a Break</h2>
                 <div className="break-btn">
-                <button>10s</button>
-                <button>20s</button>
-                <button>30s</button>
-                <button>40s</button>
-                <button>50s</button>
+                <button onClick={()=> handleBreakTime(10)}>10s</button>
+                <button onClick={()=> handleBreakTime(20)}>20s</button>
+                <button onClick={()=> handleBreakTime(30)}>30s</button>
+                <button onClick={()=> handleBreakTime(40)}>40s</button>
+                <button onClick={()=> handleBreakTime(50)}>50s</button>
                 </div>
              </div>
 
@@ -54,7 +60,7 @@ const TodoStats = ({times}) => {
                 </div>
                 <div className="break-time">
                     <h4>Break Time </h4>
-                    <p>20 min</p>
+                    <p>{breakTime} min</p>
                 </div>
              </div>
              <div className="activity-done">
